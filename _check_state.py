d358d2b34aa77bb9b23c6525e14bc01b83fc0f5e
Kwame Sefah <kwamesefah+microsoft@microsoft.com>
2026-04-26 14:56:52 -0500

    chore: move temp _*.py scripts to _scratch/ and gitignore them

--- _check_state.py
+++
@@ -1,9 +0,0 @@
-import json
-nb = json.load(open('Healthcare_Launcher.ipynb', encoding='utf-8'))
-for i, c in enumerate(nb['cells']):
-    src = ''.join(c.get('source', []))
-    first = src.strip().split('\n')[0][:100]
-    nlines = len(c['source'])
-    has_sc = ' ** HAS SHORTCUTS **' if 'CREATING ONELAKE SHORTCUTS' in src else ''
-    has_box = ' ** HAS BOX **' if 'TWO QUICK STEPS' in src else ''
-    print(f"Cell {i} ({nlines} lines){has_sc}{has_box}: {first}")
